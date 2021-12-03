6bd6e1de0bb9931e31f0b98ba0a46b71477cd0e9
Christian Kontz <ckontz@stanford.edu>
2021-12-02 17:37:59 -0800

    updates

--- texbuilder.sh
+++ texbuilder.sh
@@ -1,7 +1,7 @@
 # @Author: ChristianKontz
 # @Date:   2020-11-23 14:30:36
-# @Last Modified by:   ChristianKontz
-# @Last Modified time: 2020-11-23 14:55:48
+# @Last Modified by:   Christian Kontz
+# @Last Modified time: 2021-04-25 16:48:47
 function tex_pdf {
 	compiler=xelatex
 	
@@ -12,7 +12,7 @@ function tex_pdf {
 
 		if grep -q 'pycode' $1.tex; then
 			printf "\nMaking a quick stop to compile embedded Python code... \n"
-			pythontex $1.tex
+			pythontex --interpreter python:/opt/anaconda3/bin/python $1.tex
 
 			$compiler -halt-on-error -interaction=nonstopmode -shell-escape $1.tex > $1.txt
 			grep '^!.*' --color=never $1.txt
@@ -34,7 +34,15 @@ function tex_pdf {
 		# $compiler -halt-on-error -interaction=nonstopmode -shell-escape $1.tex > $1.txt
 		# grep '^!.*' --color=never $1.txt
 	else
+		printf "\nStep 1/3 - Compiling TeX file... (using pdflatex)"
 		pdflatex -synctex=1 -interaction=nonstopmode $1.tex > $1.txt
+		if ls | grep -q '.bib'; then 
+			printf "\nStep 2/3 - Compiling BibTeX..."
+			bibtex $1.aux > $1.txt
+			printf "\nStep 3/3 - Compiling TeX file again... "
+			pdflatex -synctex=1 -interaction=nonstopmode $1.tex > $1.txt
+			# pdflatex -synctex=1 -interaction=nonstopmode $1.tex > $1.txt
+		fi
 		grep '^!.*' --color=never $1.txt
 	fi
 
