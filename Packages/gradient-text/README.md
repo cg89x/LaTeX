e3d488bfc6d0b58c9134b1eb2c65efd8ae8711c8
Christian Kontz <ckontz@stanford.edu>
2023-05-27 13:25:42 -0700

    update tex

---
+++ Packages/gradient-text/README.md
@@ -0,0 +1,38 @@
+gradient-text
+=========
+
+The 'gradient-text' package enables writers to conveniently decorate text with linear
+gradient colors.
+
+Basic Usage
+-----------
+The package only provides a user command.
+
+    \gradientRGB {⟨text⟩}{⟨first RGB⟩}{⟨last RGB⟩}
+
+You can read the package manual for some explanations.
+
+Copyright and License
+---------------------
+
+    Copyright (C) 2022-2023 by Sicheng Du <siddsc@foxmail.com>
+    -----------------------------------------------------------------
+
+    This work may be distributed and/or modified under the
+    conditions of the LaTeX Project Public License, either
+    version 1.3c of this license or (at your option) any later
+    version. This version of this license is in
+       http://www.latex-project.org/lppl/lppl-1-3c.txt
+    and the latest version of this license is in
+       http://www.latex-project.org/lppl.txt
+    and version 1.3 or later is part of all distributions of
+    LaTeX version 2005/12/01 or later.
+
+    This work has the LPPL maintenance status "maintained".
+
+    The Current Maintainer of this work is Sicheng Du.
+
+    This package consists of the files  gradient-text.tex,
+                                        gradient-text.pdf,
+                                        gradient-text.sty and
+                                        README.md (this file).
--- texbuilder.sh
+++ texbuilder.sh
@@ -1,20 +1,27 @@
-# @Author: ChristianKontz
-# @Date:   2020-11-23 14:30:36
+# @Author: Christian Kontz
+# @Date:   2023-05-27 13:24:42
 # @Last Modified by:   Christian Kontz
-# @Last Modified time: 2021-04-25 16:48:47
+# @Last Modified time: 2023-05-27 13:24:52
+
 function tex_pdf {
 	compiler=xelatex
-	
-	if grep -E -q 'pycode|.jl|svg' $1.tex; then
+	# compiler=lualatex
+
+	# if grep -E -q 'gemini' $1.tex; then
+	if grep -E -q 'gemixiXXXXXxx' $1.tex; then
+		latexmk -pdflatex='lualatex -interaction nonstopmode' -pdf $1.tex
+		# latexmk -pdf -C
+		# $compiler -halt-on-error -interaction=nonstopmode -shell-escape -synctex=1 $1.tex > $1.txt 
+	elif grep -E -q 'gemini|pycode|.jl|svg' $1.tex; then
 		printf "\nStep 1/3 - Compiling TeX file... (using $compiler) \n"
-		$compiler -halt-on-error -interaction=nonstopmode -shell-escape $1.tex > $1.txt 
+		$compiler -halt-on-error -interaction=nonstopmode -shell-escape -synctex=1 $1.tex > $1.txt 
 		grep '^!.*' --color=never $1.txt
 
 		if grep -q 'pycode' $1.tex; then
 			printf "\nMaking a quick stop to compile embedded Python code... \n"
 			pythontex --interpreter python:/opt/anaconda3/bin/python $1.tex
 
-			$compiler -halt-on-error -interaction=nonstopmode -shell-escape $1.tex > $1.txt
+			$compiler -halt-on-error -interaction=nonstopmode -shell-escape -synctex=1 $1.tex > $1.txt
 			grep '^!.*' --color=never $1.txt
 		fi
 
@@ -24,23 +31,23 @@ function tex_pdf {
 			grep '^!.*' --color=never $1.txt
 
 			printf "\nStep 3/3 - Compiling TeX file again... \n"
-			$compiler -halt-on-error -interaction=nonstopmode -shell-escape $1.tex > $1.txt
+			$compiler -halt-on-error -interaction=nonstopmode -shell-escape -synctex=1 $1.tex > $1.txt
 			grep '^!.*' --color=never $1.txt
 		else
 			printf "\nSkipping Step 2 and 3 - b/c no .bib file found \n"
 		fi
 
 		# printf "\nStep 4/4 - Compiling TeX file again... \n"
-		# $compiler -halt-on-error -interaction=nonstopmode -shell-escape $1.tex > $1.txt
+		# $compiler -halt-on-error -interaction=nonstopmode -shell-escape -synctex=1 $1.tex > $1.txt
 		# grep '^!.*' --color=never $1.txt
 	else
 		printf "\nStep 1/3 - Compiling TeX file... (using pdflatex)"
-		pdflatex -synctex=1 -interaction=nonstopmode $1.tex > $1.txt
+		pdflatex -shell-escape -synctex=1 -interaction=nonstopmode $1.tex > $1.txt
 		if ls | grep -q '.bib'; then 
 			printf "\nStep 2/3 - Compiling BibTeX..."
 			bibtex $1.aux > $1.txt
 			printf "\nStep 3/3 - Compiling TeX file again... "
-			pdflatex -synctex=1 -interaction=nonstopmode $1.tex > $1.txt
+			pdflatex -shell-escape -synctex=1 -interaction=nonstopmode $1.tex > $1.txt
 			# pdflatex -synctex=1 -interaction=nonstopmode $1.tex > $1.txt
 		fi
 		grep '^!.*' --color=never $1.txt
